431079af75fd64ad8a8b5b89db4c579db940fcb9
Miracle2712 <100839563+Miracle2712@users.noreply.github.com>
2022-04-04 15:12:16 +0700

    songuyento

---
+++ src/vonglapJava/lap1.java
@@ -0,0 +1,29 @@
+package vonglapJava;
+
+import java.util.Scanner;
+
+public class lap1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số:");
+        int number = scanner.nextInt();
+        if (number < 2){
+            System.out.println(number + " Không phải sồ nguyên tố");
+        } else {
+            int i = 2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if (check)
+                System.out.println(number + " Là số nguyên tố");
+            else
+                System.out.println(number + " Không phải sồ nguyên tố");
+        }
+    }
+}
